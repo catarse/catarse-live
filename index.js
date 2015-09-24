d840a0d3861ed2021f6f48991ee3c76988880a21
Antônio Roberto <forevertonny@gmail.com>
2015-09-24 11:53:34 -0300

    adjust code style a bit

--- index.js
+++ index.js
@@ -1,51 +1,49 @@
-var logger = require('morgan');
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var pg = require('pg');
-var channel_name = 'new_paid_contributions';
-var options = {
-  dotfiles: 'ignore',
-  etag: false,
-  extensions: ['htm', 'html'],
-  index: false,
-  maxAge: '1d',
-  redirect: false,
-  setHeaders: function (res, path, stat) {
-    res.set('x-timestamp', Date.now());
-  }
-};
+var logger = require('morgan'),
+    express = require('express'),
+    app = express(),
+    server = require('http').Server(app),
+    io = require('socket.io')(server),
+    pg = require('pg'),
+    channel_name = 'new_paid_contributions',
+    options = {
+      dotfiles: 'ignore',
+      etag: false,
+      extensions: ['htm', 'html'],
+      index: false,
+      maxAge: '1d',
+      redirect: false,
+      setHeaders: function (res, path, stat) {
+        res.set('x-timestamp', Date.now());
+      }
+    },
+    pgClient = new pg.Client(process.env.DATABASE_URL);
 
+// APP BASIC CONFIG
 app.use(express.static('public', options));
 app.use(logger('short'));
-
 app.set('view engine', 'jade');
 app.set('port', (process.env.PORT || 5000));
 
+// APP ROUTES
 app.get('/', function (req, res) {
   res.render('index', { title: 'Hey', message: 'Hello there!'});
 });
+
 server.listen(app.get('port'));
 
-var client = new pg.Client(process.env.DATABASE_URL);
-client.connect(function(err) {
+// POSTGRESQL CLIENT CONNECTION STUFF
+pgClient.connect(function(err) {
  if(err) {
     return console.error('could not connect to postgres', err);
   }
-  client.on('notification', function(msg) {
-    if(msg.channel !== channel_name) return;
+  pgClient.on('notification', function(msg) {
+    if (msg.channel !== channel_name) return;
     console.log('Refreshing view');
-    //client.query('REFRESH MATERIALIZED VIEW CONCURRENTLY "1".statistics;')
+    //pgClient.query('REFRESH MATERIALIZED VIEW CONCURRENTLY "1".statistics;')
     console.log('Broadcasting notification: ', msg);
     io.of('/').sockets.forEach(function(socket){
       socket.emit(channel_name, msg);
     });
   });
-  client.query("LISTEN " + channel_name);
-});
-
-io.on('connection', function (socket) {
-  console.log('Open websocket');
-  socket.emit('connected', { hello: 'world' });
+  pgClient.query("LISTEN " + channel_name);
 });
